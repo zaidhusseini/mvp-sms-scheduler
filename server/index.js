531e7629592f446f53700fcf46ed07fcdf70bfbc
zaid <zaidhusseini@gmail.com>
2018-05-16 19:28:09 -0700

    Add Favicon and support from backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+const favicon = require('serve-favicon');
+const path = require('path');
 
+app.use(favicon(path.join(__dirname,'..', 'client', 'public','images','favicon.ico')));
 app.use(express.static('./client/public'));
 
 app.use('/send', router);
